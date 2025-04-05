3d279f881b2d2a2434edd1ce621ab678d486890b
Shoban-Ravichandran-23272040 <x23272040@student.ncirl.ie>
2025-04-05 21:41:02 +0100

    Update setup.py

--- CA/intelligent_invoice_processor/setup.py
+++ CA/intelligent_invoice_processor/setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 setup(
     name="intelligent-invoice-processor",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Shoban Ravichandran",
+    author_email="x23272040@student.ncirl.ie",
     description="An intelligent OCR and ML-based invoice processing system",
     long_description=long_description,
     long_description_content_type="text/markdown",
